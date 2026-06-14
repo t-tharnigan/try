949db07e08bc7452efd267d7a6e566e71e2fec93
t-tharnigan <tharniganthambibe@gmail.com>
2026-06-14 23:55:58 +0200

    Nieuwe feature

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+cat myCat = new cat();
+myCat.name = "Whiskers";
+myCat.age = 3;
+
+
+
+myCat.meow(); // Call meow() on the instance myCat
+
+class cat
+{
+    public string name;
+    public int age;
+
+    public void meow()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
